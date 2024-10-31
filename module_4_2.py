feca7403b277ef18cd1a031b33d7236621e20400
Anton1109 <AntFom19941109@yandex.ru>
2024-10-31 20:45:23 +0300

    Update module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -1,15 +1,14 @@
 def test_function():
     def inner_function():
         print("Я в области видимости функции test_function")
-
-    try: inner_function()
-
-    except NameError as e:
-
-     print(e)  # NameError: name 'inner_function' is not defined
-
-
+        inner_function()
 test_function()
+try:
 
+    inner_function()
+
+except NameError as e:
+
+print(e) # NameError: name 'inner_function' is not defined
 
 
