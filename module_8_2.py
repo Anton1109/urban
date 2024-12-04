fe3cf38d96db3bd6776c73d5b04a8cb2fb88fdad
Anton1109 <AntFom19941109@yandex.ru>
2024-12-04 19:27:54 +0300

    Add files via upload

--- module_8_2.py
+++ module_8_2.py
@@ -1,47 +1,42 @@
-def personal_sum(numbers):
-    result = 0 # Сумма чисел
-    incorrect_data = 0 # Счетчик некорректных данных
+def personal_sum(*numbers):
+    result = 0
+    incorrect_data = 0
 
-    for item in numbers:
-        try:
-            result += int(item) # Пробуем преобразовать в число и добавить к результату
-        except (ValueError, TypeError): # Обрабатываем исключения, если элемент некорректный
-            incorrect_data += 1
+    for i in numbers:
+       for j in i:
 
-    return result, incorrect_data # Возвращаем кортеж
-
-def calculate_average(numbers):
-
-    try:
-
-        if not isinstance(numbers, (list, tuple, set)):  # Проверяем, является ли входной аргумент коллекцией
-            raise TypeError('В numbers записан некорректный тип данных')
-
-
-    except TypeError:
-        print("Некорректный тип данных")
-
-
-        total_sum, incorrect_data = personal_sum(numbers) # Вызываем функцию personal_sum для обработки элементов
-
-        try:
-            valid_count = len(numbers) - incorrect_data
-            average = total_sum / valid_count  # Вычисляем среднее, исключая деление на 0
-            return average
-        except ZeroDivisionError:
-
-            return 0
-
-
-
-    except TypeError:
-        print("В numbers записан некорректный тип данных")
+            try:
+                result += j
+            except TypeError:
+                incorrect_data += 1
+                print(f'некорректный тип данных для подсчета суммы - {j}')
+    return result, incorrect_data
+def calculate_average(*numbers):
+    if isinstance(*numbers, int):
         return None
+    try:
+        tuple_pers_sum = personal_sum(*numbers)
 
-print(f'Результат 1: {personal_sum("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+        return tuple_pers_sum[0] / (len(*numbers) - tuple_pers_sum[1])
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        return f'В numbers записан некорректный тип данных'
 
-print(f'Результат 2: {personal_sum([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
 
-print(f'Результат 3: {personal_sum([567] )}') # Передана не коллекция
 
-print(f'Результат 4: {personal_sum([42, 15, 36, 13])}') # Всё должно работать
+
+
+
+
+
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
+
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
+
+print(f'Результат 3: {calculate_average([567])}')  # Передана не коллекция
+
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
+
+
